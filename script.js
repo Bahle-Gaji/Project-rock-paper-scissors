06b9da4264bace69fb50870553f710264ae835cb
Bahle Gaji <bahle.gaji01@gmail.com>
2022-11-07 11:12:34 +0200

    Create singleRound function to see who wins.

--- script.js
+++ script.js
@@ -5,7 +5,32 @@ function getComputerChoice(){
 
     let choice = options[Math.floor(Math.random() * 3)]
 
-    console.log(choice)
+    return choice;
+}
+
+getComputerChoice();
+
+function singleRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+
+        return `It's a tie! Both chose ${playerSelection}`;
+    }
+    else if(
+        (playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') 
+    ){
+
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
 
 }
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(singleRound(playerSelection, computerSelection));
 
